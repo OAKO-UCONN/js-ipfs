6fd32f4374db21f1d3817a363e5bf80216dc195e
achingbrain <alex@achingbrain.net>
2020-06-29 19:20:39 +0100

    chore: fix tests

--- packages/ipfs/src/core/components/dag/get.js
+++ packages/ipfs/src/core/components/dag/get.js
@@ -3,9 +3,15 @@
 const { withTimeoutOption } = require('../../utils')
 const first = require('it-first')
 const last = require('it-last')
+const CID = require('cids')
+const errcode = require('err-code')
 
 module.exports = ({ ipld, preload }) => {
   return withTimeoutOption(async function get (cid, options = {}) {
+    if (!CID.isCID(cid)) {
+      throw errcode(new Error('Invalid CID'), 'ERR_INVALID_CID')
+    }
+
     if (options.preload !== false) {
       preload(cid)
     }
